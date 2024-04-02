d4780aac9bd053ef1e538f98bd03547051458708
Vladtsrez <Competitionvladinf@gmail.com>
2024-04-03 00:11:18 +0300

    add

---
+++ vite-project/main.js
@@ -0,0 +1,11 @@
+import validator from 'validator';
+
+console.log(
+  'Is mango@mail.com a valid email?: ',
+  validator.isEmail('mango@mail.com')
+); // Is mango@mail.com a valid email?: true
+
+console.log(
+  'Is Mangodogmail.com a valid email?: ',
+  validator.isEmail('Mangodogmail.com')
+); // Is mango@mail.com a valid email?: false
